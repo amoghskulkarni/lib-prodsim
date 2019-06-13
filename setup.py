aa03e86fd0b2eab0a2aa4ce46b8f6b85600f9bf4
Amogh Kulkarni <amogh.s.kulkarni@vanderbilt.edu>
2019-06-13 16:35:02 -0500

    Added necessary files for Python module

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prodsim",
+    version="0.0.1",
+    author="Amogh Kulkarni",
+    author_email="amoghkulkarni.kop@gmail.com",
+    description="A small library for building production line simulations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amoghskulkarni/python-example-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
